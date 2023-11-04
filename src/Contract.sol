10fa27da1c3df95913853b467552260c5b072a21
Sardius <0xsardius@gmail.com>
2023-11-04 09:36:34 -0400

    simple setup complete

--- src/Contract.sol
+++ src/Contract.sol
@@ -1,6 +1,19 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-contract Contract {
-    constructor() {}
+contract HelloWorld {
+   string public message;
+
+    constructor() {
+        message = "gm";
+    }
+
+   function setMessage(string memory _message) public {
+      message = _message;
+   }
+
+   function getMessage() public view returns (string memory) {
+      return message;
+   }
+    
 }
